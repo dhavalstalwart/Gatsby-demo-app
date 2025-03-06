7518a64fe01d510ee05729b9299ef163422bab40
Dhaval-stalwart-developer <dhaval.stalwartdigital@gmail.com>
2025-03-06 12:18:20 +0530

    display blog file names using gatsby-source-filesystem

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,8 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import { graphql, Link, useStaticQuery } from 'gatsby'
 import {
   container,
+  siteTitle,
   heading,
   navLinks,
   navLinkItem,
@@ -9,8 +10,18 @@ import {
 } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
+    const data = useStaticQuery(graphql `query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }`)
+
+   
   return (
     <div className={container}>
+            <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
@@ -21,6 +32,11 @@ const Layout = ({ pageTitle, children }) => {
           <li className={navLinkItem}>
             <Link to="/about" className={navLinkText}>
               About
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/blog" className={navLinkText}>
+              Blog
             </Link>
           </li>
         </ul>
