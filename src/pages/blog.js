612428655ef32bd008b50240b5f83fb132db4736
Dhaval-stalwart-developer <dhaval.stalwartdigital@gmail.com>
2025-03-06 13:04:58 +0530

    Displayed actual blog post content by using gatsby-plugin-mdx

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,36 +1,46 @@
-import * as React from 'react'
-import Layout from '../components/layout'
-import Seo from '../components/Seo'
-import { graphql } from 'gatsby'
+import * as React from "react";
+import Layout from "../components/layout";
+import Seo from "../components/Seo";
+import { graphql } from "gatsby";
 
-const BlogPage = ({data}) => {
-   
+const BlogPage = ({ data }) => {
+  const BlogPosts = data.allMdx.nodes;
+
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
-      {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
-        ))
-      }
-      </ul>
+      {BlogPosts.length > 0 ? (
+        BlogPosts.map((node) => {
+          return (
+            <article key={node.id}>
+              <h2>{node.frontmatter.title}</h2>
+              <p>Posted: {node.frontmatter.date}</p>
+              <p>{node.excerpt}</p>
+            </article>
+          );
+        })
+      ) : (
+        <p>Blog Post Not Exists!</p>
+      )}
     </Layout>
-  )
-}
+  );
+};
 
 export const query = graphql`
-query {
-  allFile(sort: {atime: DESC}, filter: {sourceInstanceName: {eq: "blog"}}) {
-    nodes {
-      name
+  query {
+    allMdx(sort: { frontmatter: { date: DESC } }) {
+      nodes {
+        frontmatter {
+          title
+          slug
+          date(formatString: "MMMM D, YYYY")
+        }
+        id
+        excerpt
+      }
     }
   }
-}
-`
+`;
 
-export const Head = () => <Seo title="My Blog Posts" />
+export const Head = () => <Seo title="My Blog Posts" />;
 
-export default BlogPage
-
+export default BlogPage;
