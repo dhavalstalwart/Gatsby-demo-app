52fe1a8beaa320010f4fe7c97b0c45963bdcd787
Dhaval-stalwart-developer <dhaval.stalwartdigital@gmail.com>
2025-03-07 13:12:46 +0530

    Navigate using Gatsby Link API

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,32 @@
-import * as React from 'react'
-import Layout from '../components/layout'
-import Seo from '../components/Seo'
+import * as React from "react";
+import Layout from "../components/layout";
+import Seo from "../components/Seo";
+import { navigate } from "gatsby";
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+      </p>
+      <button
+        type="button"
+        onClick={() => navigate("/")}
+        style={{
+          background: "green",
+          color: "white",
+          cursor: "pointer",
+          padding: "10px",
+          borderRadius: "5px",
+          borderColor: "1px solid gray",
+        }}
+      >
+        Back To Home Page
+      </button>
     </Layout>
-  )
-}
+  );
+};
 
-export const Head = () => <Seo title="About Me" />
+export const Head = () => <Seo title="About Me" />;
 
-export default AboutPage
+export default AboutPage;
